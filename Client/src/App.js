f0dbd562e2d2a435cda15d1fccc1e0e9820b15f6
aasifwali16@gmail.com <aasifwali16@gmail.com>
2024-02-13 01:40:07 +0530

    navbar and footer complete

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import {Home} from "./Pages/Home";
+import {About} from "./Pages/About";
+import {Cart} from "./Pages/Cart";
+import {Login} from "./Pages/Login";
+import {Register} from "./Pages/Register";
+import {Products} from "./Pages/Products";
+import { Footer } from "./Components/Footer";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/products" element={<Products/>}/>
+      <Route path="/cart" element={<Cart />} />
+      
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+      
     </div>
   );
 }
 
-export default App;
---
+++ Client/src/Pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Register = () => {
+  return (<div className="relative h-screen mb-16">
+  <img src="https://1.bp.blogspot.com/-1zIi9LZENIo/WYbdzs6AFEI/AAAAAAAALFY/Fwvv2TE2cwIqiWMLLaxgKlVJGwqoar4hgCLcBGAs/s1600/bye-bye-builder.jpg" alt="Builder" className=" w-full h-screen mb-16 object-cover overflow-x-hidden" />
+  <div className="absolute inset-0 flex items-start mt-6 justify-center">
+    <h1 className=" text-white text-center font-bold text-4xl">This Page is Under Construction !</h1>
+  </div>
+</div>
+  )
+}
