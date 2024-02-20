33f88c25dbf2d5030e24050c547ef59445ac147e
aasifwali16@gmail.com <aasifwali16@gmail.com>
2024-02-20 16:50:15 +0530

    filter by category added

--- Client/src/Pages/Products.jsx
+++ Client/src/Pages/Products.jsx
@@ -22,31 +22,38 @@ export const Products = () => {
 
       return<section className="text-gray-600 body-font overflow-x-hidden">
       {/*Accordian*/}
-      <h2 id="accordion-collapse-heading-1" onClick={ToggleOptionsVisibility}>
-      <button type="button" className="flex items-center justify-between w-full py-4 px-12 m-0 rtl:text-right text-white  text-l font-semibold bg-gray-900 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
-        <span>Categories</span>
-        <svg data-accordion-icon className={`w-3 h-3 rotate-${showOptions? '0' : '180'} shrink-0`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6" onClick={ToggleOptionsVisibility}>
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-        </svg>
-      </button>
-    </h2>
-    <div id="accordion-collapse-body-1" className={` ${showOptions?'' : 'hidden'} relative`} aria-labelledby="accordion-collapse-heading-1">
-        <div className='absolute z-10 w-full'>
-          <div className="px-8 py-4 flex  lg:flex-col items-center justify-center  gap-2 bg-cyan-700">
-           <button className="bg-gray-900  w-full text-white text-gray-300 hover:bg-Primary hover:text-white
-           block rounded-md px-3 py-2 text-base font-medium">T-Shirt</button>
-           <button className="bg-gray-900  w-full text-white text-gray-300 hover:bg-Primary hover:text-white
-           block rounded-md px-3 py-2 text-base font-medium" >Hoodies</button>
-           <button className="bg-gray-900  w-full text-white text-gray-300 hover:bg-Primary hover:text-white
-           block rounded-md px-3 py-2 text-base font-medium">Mugs</button>
-           <button className="bg-gray-900  w-full text-white text-gray-300 hover:bg-Primary hover:text-white
-           block rounded-md px-3 py-2 text-base font-medium">Bottles</button>
-           <button className="bg-gray-900  w-full text-white text-gray-300 hover:bg-Primary hover:text-white
-           block rounded-md px-3 py-2 text-base font-medium">Mouse Pads</button>
+    
 
-          </div>
-        </div>
+    
+<button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white w-full py-4 px-12 m-0 text-white  text-center font-semibold bg-gray-900 gap-3 font-medium  text-sm px-5 py-2.5 text-center flex items-center justify-center" type="button" onClick={ToggleOptionsVisibility}>Filter by Category<svg className={`w-2.5 h-2.5 ms-3 rotate-${showOptions?"180":0}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+</svg>
+
+</button>
+
+
+<div id="dropdown" className={`${showOptions?'' : 'hidden'} relative px-auto w-full bg-cyan-600 divide-y divide-gray-100 shadow  `}>
+<div className='absolute z-10 bg-gray-900 w-full '>
+    <ul className="py-2 px-auto text-sm text-gray-700 lg:flex justify-center  w-full " aria-labelledby="dropdownDefaultButton">
+      <li className='m-2'>
+        <button className="block px-4 py-2 w-full ">T-Shirts</button> 
+      </li>
+      <li>
+      <button className="block px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Hoodies</button>
+      </li>
+      <li>
+      <button className="block px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mugs</button>
+      </li>
+      <li>
+      <button className="block px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Bottle</button>
+      </li>
+      <li>
+      <button className="block px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mouse Pads</button>
+      </li>
+    </ul>
     </div>
+</div>
+
     {/*Accordian*/}
       <div className="container px-5 py-16 mx-auto">
         <div className="flex flex-col text-center w-full mb-4">
