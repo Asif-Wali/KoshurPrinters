b72cdc86c53d7bd80cd228ebf4c9d08ab449fd2f
aasifwali16@gmail.com <aasifwali16@gmail.com>
2024-02-20 17:58:34 +0530

    category layout completed

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar=()=>{
         className='bg-gray-900 mb-2 w-full text-white text-gray-300 hover:bg-Primary hover:text-white
           block rounded-md px-3 py-2 text-base font-medium'>
       {/**Conditional Rendering**/}
-      Profile
+      Login
       </Disclosure.Button>
       </Link>
         </div>
--- Client/src/Pages/Products.jsx
+++ Client/src/Pages/Products.jsx
@@ -33,7 +33,7 @@ export const Products = () => {
 
 
 <div id="dropdown" className={`${showOptions?'' : 'hidden'} relative px-auto w-full divide-y divide-gray-100 shadow `}>
-<div className='absolute z-10 bg-cyan-600 rounded-md  w-full '>
+<div className='absolute z-10 bg-cyan-600 rounded-b-lg  w-full '>
     <ul className="py-2 px-4 text-sm text-gray-700 lg:flex justify-evenly  w-full " aria-labelledby="dropdownDefaultButton">
       <li className='m-2'>
         <button className="block px-4 py-2 w-full  text-white text-gray-3 bg-gray-900 hover:bg-Primary hover:text-white
@@ -62,17 +62,13 @@ export const Products = () => {
     {/*Accordian*/}
       <div className="container px-5 py-16 mx-auto">
         <div className="flex flex-col text-center w-full mb-4">
-          <h1 class bg-gray-900Name="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Products</h1> 
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Products</h1> 
         </div>
         <div className="flex flex-wrap -m-4">
           <div className="lg:w-1/3 sm:w-1/2 p-4">
             <div className="flex relative">
             
-            <div class="px-8 py-10 relative w-full border-4 border-gray-200 bg-white ">
-          bg-gray-900     <h2 class="tracking-widest text-sm title-font font-medium text-indi bg-gray-900go-500 mb-1">T-Shirt</h2>
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Kashmir</h1>
-              <p class="leading-relaxed">World class T-Shirt Design made out of premium cottom.</p>
-            </div>
+           
               
             
             </div>
