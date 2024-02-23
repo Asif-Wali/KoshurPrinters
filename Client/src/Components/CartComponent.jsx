142f87df58addf8d12ad7da5b8611bf3084d0eb0
aasifwali16@gmail.com <aasifwali16@gmail.com>
2024-02-23 20:47:17 +0530

    checkout section created

--- Client/src/Components/CartComponent.jsx
+++ Client/src/Components/CartComponent.jsx
@@ -13,7 +13,7 @@ export const CartComponent = () => {
     }
 
   return (
-    <div className='bg-white rounded-lg  w-full lg:w-3/5'>
+    <div className='bg-white rounded-lg  w-full'>
                     <div className=' px-2 flex items-center justify-start'>
                         <div className='p-4'>
                           <img className='h-24 rounded' src='https://dcassetcdn.com/design_img/22773/30063/30063_498839_22773_image.png' alt='cartProduct'/>
@@ -24,9 +24,9 @@ export const CartComponent = () => {
                             <h3 className='text-md'>Price</h3>
                         </div>
                     </div>
-                    <div className='w-full  flex items-center gap-2 p-2 justify-end pr-2'>
+                    <div className='w-full  flex items-center gap-2 p-2 justify-end pr-4'>
                       <TrashIcon className='h-5 rounded ring-gray-700 text-[red]'/>
-                      <div className='flex items-center border px-3 py rounded border-gray-500 justify-center gap-1'>
+                      <div className='flex items-center border px-3 rounded border-gray-500 justify-center gap-1'>
                         <PlusIcon className='h-4 ring-1 rounded ring-gray-500 text-[green]' onClick={()=>{
                             HandleIncrement()
                         }}/>
--- Client/src/Pages/Cart.jsx
+++ Client/src/Pages/Cart.jsx
@@ -7,22 +7,40 @@ export const Cart = () => {
   const arr = [1,2,3,4,5] 
 
   return (<div className="relative  mb-16">
-            <div  className='cart-heading p-4 bg-[#d9e7e2]'>
+            <div  className='cart-heading p-4'>
             {/*heading*/}
               <div className='p-4 bg-cyan-800 rounded-lg text-center'>
               <h1 className=' text-xl text-white flex items-center gap-2 justify-center'><ShoppingCartIcon className='h-6 w-6 mt-1 text-white'/> Cart ({arr.length}) </h1>
               </div>
               {/*body*/}
-              <div className='cart-container w-full p-2  lg:flex'>
-                <div className='cart-detail shadow-lg flex flex-col gap-2 w-full'>
+              <div className='cart-container shadow-xl  w-full p-2  lg:flex lg:gap-8'>
+                <div className='cart-detail rounded flex flex-col gap-2 w-full'>
                   {arr.map((el)=>{
                     return<CartComponent/>
                   })}
 
                 </div>
 
-               <div className='cart-total border-2 border-Gframe '>
-                </div>
+               <div className='cart-total rounded-xl shadow-xl lg:w-2/5 py-8 px-4 border border-Secondary h-auto my-2'>
+                    <div className='heading1 p-3 flex align-center justify-between'>
+                    <h1 className='font-semibold'>Total</h1>
+                    <h2>600/-</h2>
+                    </div>
+                    <div className='heading1 p-3 flex align-center justify-between'>
+                    <h1 className='font-semibold'>Shipping</h1>
+                    <h2>50/-</h2>
+                    </div>
+                    <hr className='h-0 border-b border-solid border-Primary grow'/>
+
+                    <div className='heading1 p-3 flex align-center justify-between mt-2'>
+                    <h1 className=' font-bold'>Grand Total</h1>
+                    <h2>650/-</h2>
+                    </div>
+
+                    <button className='w-full p-4 rounded-md bg-Primary mt-4 text-white font-semibold'>Checkout</button>
+
+              </div>
+
               
               </div>
             </div>
